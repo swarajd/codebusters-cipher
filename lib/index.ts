4965e74730954232c28791e12f1aa9f5d8ac4f53
swarajd <swaraj.dhumne@gmail.com>
2019-02-12 22:43:40 -0500

    added extra function for testing

--- lib/index.ts
+++ lib/index.ts
@@ -5,3 +5,11 @@
 export function cipher(x: string): string {
   return x;
 }
+
+/**
+ * add exclamation mark to string
+ * @param s string to add exclamation mark to
+ */
+export function addExclamation(s: string): string {
+  return `${s}!`;
+}
