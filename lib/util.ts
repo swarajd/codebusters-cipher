c4914627aeb480eab1981e6e6f7dfba8d76c9bd0
swarajd <swaraj.dhumne@gmail.com>
2019-02-15 15:03:17 -0500

    added comments to various utils

--- lib/util.ts
+++ lib/util.ts
@@ -1,5 +1,12 @@
+
+/**
+ * list of capitalized letters
+ */
 export const letters: string[] = Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 65));
 
+/**
+ * mapping of capitalized letters to indices (A -> 0, B -> 1, ...)
+ */
 export const letterDict: { [letter: string]: number } = letters.reduce(
   (letterMap: { [letter: string]: number }, letter: string): { [letter: string]: number } => {
     letterMap[letter] = letter.charCodeAt(0) - 65;
