668f83114436fb36b43ee26159321b14a7f4c75e
swarajd <swaraj.dhumne@gmail.com>
2019-02-15 18:19:02 -0500

    more scaffolding

---
+++ lib/monoalphabetic.ts
@@ -0,0 +1,11 @@
+import { isLetter } from './util';
+
+/**
+ * function to encrypt a string using using the monoalphabetic cipher
+ * @param input the input string to encrypt
+ * @param map the map to encrypt with (optional)
+ * @param alphabetType the alphabet type to use when encrypting (optional)
+ */
+export function monoalphabetic(input: string, map?: { [letter: string]: string }, alphabetType?: string) {
+  return input;
+}
