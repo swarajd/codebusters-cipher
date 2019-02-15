fff5f5253de3bae7956ca29638db1af6f177b52c
swarajd <swaraj.dhumne@gmail.com>
2019-02-15 17:52:33 -0500

    updated shiftLetter to use a helper function isLetter

--- lib/util.ts
+++ lib/util.ts
@@ -4,6 +4,11 @@
 export const letters: string[] = Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 65));
 
 /**
+ * set of capitalized letters
+ */
+export const letterSet: Set<string> = new Set(letters);
+
+/**
  * mapping of capitalized letters to indices (A -> 0, B -> 1, ...)
  */
 export const letterDict: { [letter: string]: number } = letters.reduce(
@@ -13,6 +18,14 @@ export const letterDict: { [letter: string]: number } = letters.reduce(
   },
   {},
 );
+
+/**
+ * check if a character is a letter
+ * @param character character to check membership in letter set
+ */
+export function isLetter(character: string) {
+  return letterSet.has(character);
+}
 
 /**
  * utility to shift a capitalized letter by a certain amount
@@ -25,14 +38,20 @@ export const letterDict: { [letter: string]: number } = letters.reduce(
  * @param shift the amount we want to shift by
  */
 export function shiftLetter(letter: string, shift: number): string {
-  const idx: number = letterDict[letter.toUpperCase()];
-
-  // return the same letter if it doesn't exist in the dictionary
-  if (idx === undefined) {
+  // return the same letter if it doesn't exist
+  if (!isLetter(letter)) {
     return letter;
   }
 
+  // convert the letter to a number (ex. A -> 0)
+  const idx: number = letterDict[letter.toUpperCase()];
+
+  // calculate the new index
   const newIdx: number = (idx + shift) % letters.length;
+
+  // convert the new index to a letter
   const result = letters[newIdx];
+
+  // return the letter
   return result;
 }
