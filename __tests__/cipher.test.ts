493778df8b81747e9dfaed3fe35ed34e142f5ec0
swarajd <swaraj.dhumne@gmail.com>
2019-02-15 16:22:00 -0500

    cleaning up index to work properly

--- __tests__/cipher.test.ts
+++
@@ -1,6 +0,0 @@
-import { cipher } from '../lib/index';
-
-// generic identity cipher test
-test('My Cipher', () => {
-  expect(cipher('x')).toBe('x');
-});
--- lib/index.ts
+++ lib/index.ts
@@ -1,15 +1,1 @@
-/**
- * basic identity cipher
- * @param x string to be encrypted
- */
-export function cipher(x: string): string {
-  return x;
-}
-
-/**
- * add exclamation mark to string
- * @param s string to add exclamation mark to
- */
-export function addExclamation(s: string): string {
-  return `${s}!`;
-}
+export { caesar } from './caesar';
